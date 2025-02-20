65fa9555c36ffd4ed0e5cbf3c0a5494d76c2fe30
Wanderson notebook <wandersonlife7@gmail.com>
2025-02-20 17:02:10 -0300

    using cors

--- backend/server.ts
+++ backend/server.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import routes from "./routes/Router";
+import cors from "cors";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 
 const app = express();
+
+app.use(cors({ origin: "https://the-news-2a20.onrender.com" }));
 app.use(express.json());
 
 app.use(routes);
