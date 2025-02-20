77fb6daa2cbc52597e4f7bd1cddd9a3cf20dbec9
Wanderson notebook <wandersonlife7@gmail.com>
2025-02-20 20:37:43 -0300

    router top-readers

--- backend/routes/Router.ts
+++ backend/routes/Router.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { subscribe } from "../controllers/userController";
 import { processWebhook } from "../controllers/webhookController";
-import { metrics } from "../controllers/dashboardController";
+import { metrics, topReaders } from "../controllers/dashboardController";
 
 const router = Router();
 
 router.get("/create", subscribe);
 router.get("/webhook", processWebhook);
 router.get("/metrics", metrics);
+router.get("/top-readers", topReaders);
 
 export default router;
