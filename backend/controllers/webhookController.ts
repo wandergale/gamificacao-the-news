afa8e7768598dff7ca9c61062a267d740e98b230
Wanderson notebook <wandersonlife7@gmail.com>
2025-02-19 22:10:00 -0300

    changig all logic

--- backend/controllers/webhookController.ts
+++ backend/controllers/webhookController.ts
@@ -1,87 +1,53 @@
 import { Request, Response } from "express";
-
 import pool from "../config/db";
 
-interface IWebHookRequestBody {
-  email: string;
-  newsletter_id: string;
-  opened_at: string;
-  utm_source?: string;
-  utm_medium?: string;
-  utm_campaign?: string;
-  utm_channel?: string;
-}
-
-export const handleWebhook = async (
-  req: Request<{}, {}, IWebHookRequestBody>,
-  res: Response
-): Promise<void> => {
-  const {
-    email,
-    newsletter_id,
-    opened_at,
-    utm_source,
-    utm_medium,
-    utm_campaign,
-    utm_channel,
-  } = req.body;
-
+export const processWebhook = async (req: Request, res: Response) => {
   try {
-    console.log("EMAIL RECEBIDO: ", email);
-    const result = await pool.query(`SELECT * FROM users WHERE email = $1`, [
+    const { email, id, utm_source, utm_medium, utm_campaign, utm_channel } =
+      req.body;
+
+    let user = await pool.query("SELECT * FROM users WHERE email = $1", [
       email,
     ]);
-
-    if (result.rows.length === 0) {
-      console.log("Usuário não encontrado");
-      await pool.query(`INSERT INTO users (email) VALUES ($1)`, [email]);
+    if (user.rows.length === 0) {
+      user = await pool.query(
+        "INSERT INTO users (email) VALUES ($1) RETURNING *",
+        [email]
+      );
     }
+    const userId = user.rows[0].id;
+
+    await pool.query("INSERT INTO reads (user_id, post_id) VALUES ($1, $2)", [
+      userId,
+      id,
+    ]);
 
     await pool.query(
-      `INSERT INTO reads (user_id, newsletter_id, opened_at, utm_source, utm_medium, utm_campaign, utm_channel) ` +
-        `VALUES ((SELECT id FROM users WHERE email = $1), $2, $3, $4, $5, $6, $7
-        )`,
-      [
-        email,
-        newsletter_id,
-        opened_at,
-        utm_source,
-        utm_medium,
-        utm_campaign,
-        utm_channel,
-      ]
+      `INSERT INTO streaks (user_id, current_streak, longest_streak, last_read)
+        VALUES ($1, 1, 1, NOW())
+        ON CONFLICT (user_id) DO UPDATE
+        SET 
+            current_streak = CASE 
+                WHEN streaks.last_read IS NULL OR streaks.last_read < NOW() - INTERVAL '2 days' THEN 1
+                ELSE streaks.current_streak + 1
+            END,
+            longest_streak = GREATEST(streaks.longest_streak, streaks.current_streak + 1),
+            last_read = NOW()
+        RETURNING *`,
+      [userId]
     );
 
-    const streakResult = await pool.query(
-      `SELECT * FROM streaks WHERE user_id = (SELECT id FROM users WHERE email = $1)`,
-      [email]
-    );
-
-    if (streakResult.rows.length > 0) {
-      const lastStreakDate = streakResult.rows[0].last_streak_date;
-      const currentStreak = streakResult.rows[0].current_streak;
-
-      const yesterday = new Date();
-      yesterday.setDate(yesterday.getDay() - 1);
-      const yesterdayString = yesterday.toISOString().split("T")[0];
-
-      if (lastStreakDate === yesterdayString) {
-        await pool.query(
-          `UPDATE streaks SET current_streak = ${
-            currentStreak + 1
-          } WHERE user_id = (SELECT id FROM users WHERE email = $1)`,
-          [email]
-        );
-      } else {
-        await pool.query(
-          `INSERT INTO streaks (user_id, current_streak, last_streak_date) VALUES ((SELECT id FROM users WHERE email = $1), 1, $2)`,
-          [email, opened_at.split("T")[0]]
-        );
-      }
+    if (utm_source || utm_medium || utm_campaign || utm_channel) {
+      await pool.query(
+        `INSERT INTO utm_data (user_id, utm_source, utm_medium, utm_campaign, utm_channel)
+                            VALUES ($1, $2, $3, $4, $5)`,
+        [userId, utm_source, utm_medium, utm_campaign, utm_channel]
+      );
     }
-    res.status(200).json({ message: "Dados processados" });
+
+    res.status(200).json({ message: "Webhook processed" });
   } catch (error) {
-    console.error(`Error ao processar webhook: ${error}`);
-    res.status(500).json({ message: "Erro ao processar dados" });
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
