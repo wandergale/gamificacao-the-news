9d8a741bdf14a4112bd46f171aa443215fb15d52
Wanderson notebook <wandersonlife7@gmail.com>
2025-02-20 17:06:32 -0300

    contoller to create user if email is available

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -1,11 +1,32 @@
 import { Request, Response } from "express";
 import pool from "../config/db";
 
-export const getUsers = async (req: Request, res: Response) => {
+export const subscribe = async (req: Request, res: Response) => {
+  const { email } = req.query;
+  if (!email) {
+    return res.status(400).json({ error: "Email is required" });
+  }
+
   try {
-    const result = await pool.query("SELECT * FROM users");
-    res.json(result.rows);
+    const userResult = await pool.query(
+      "SELECT id FROM users WHERE email = $1",
+      [email]
+    );
+
+    if (userResult.rows.length === 0) {
+      // se nao existir usuario
+      await pool.query(
+        `INSERT INTO users (email, created_at) VALUES ($1, NOW())`,
+        [email]
+      );
+
+      res.status(200).json({ message: "User created" });
+    } else {
+      // se ja existir
+      res.status(400).json({ error: "Email already used" });
+    }
   } catch (error) {
+    console.log(error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
