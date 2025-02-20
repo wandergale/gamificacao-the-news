bd28b989d27dae00fa95220e041e499f1f4fd10b
Wanderson notebook <wandersonlife7@gmail.com>
2025-02-20 20:52:01 -0300

    new stats route

--- backend/routes/Router.ts
+++ backend/routes/Router.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { subscribe } from "../controllers/userController";
 import { processWebhook } from "../controllers/webhookController";
-import { metrics, topReaders } from "../controllers/dashboardController";
+import { metrics, topReaders, stats } from "../controllers/dashboardController";
 
 const router = Router();
 
@@ -9,5 +9,6 @@ router.get("/create", subscribe);
 router.get("/webhook", processWebhook);
 router.get("/metrics", metrics);
 router.get("/top-readers", topReaders);
+router.get("/stats", stats);
 
 export default router;
