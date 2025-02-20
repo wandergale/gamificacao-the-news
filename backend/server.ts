80ab1cec96f7f601c2abac7c1eb2bc4e7a5b548f
Wanderson notebook <wandersonlife7@gmail.com>
2025-02-19 22:11:41 -0300

    fixing roue

--- backend/server.ts
+++ backend/server.ts
@@ -3,11 +3,12 @@ import dotenv from "dotenv";
 import routes from "./routes/Router";
 
 dotenv.config();
-const port = process.env.PORT;
+
+const PORT = process.env.PORT;
 
 const app = express();
-
 app.use(express.json());
-app.use("/", routes);
 
-app.listen(port, () => console.log(`Server rodando na porta ${port}`));
+app.use(routes);
+
+app.listen(PORT, () => console.log(`Server rodando na porta ${PORT}`));
