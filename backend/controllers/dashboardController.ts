463a28e2ab30ce6f99f9ba3ec5645d6a8afaf677
Wanderson notebook <wandersonlife7@gmail.com>
2025-02-20 20:38:05 -0300

    top-readers rank route

--- backend/controllers/dashboardController.ts
+++ backend/controllers/dashboardController.ts
@@ -4,8 +4,22 @@ import pool from "../config/db";
 export const metrics = async (req: Request, res: Response) => {
   try {
     const result = await pool.query("SELECT * FROM engagement_metrics");
-    res.json(result.rows[0])
+    res.json(result.rows[0]);
   } catch (error) {
-    res.status(500).json({error: "Error on searching metrics"})
+    res.status(500).json({ error: "Error on searching metrics" });
+  }
+};
+
+export const topReaders = async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`
+            SELECT u.email, s.current_streak, s.longest_streak, s.last_read
+            FROM streaks s 
+                JOIN users u ON u.id = s.user_id
+                ORDER BY s.current_streak DESC LIMIT 10`);
+
+    res.json(result.rows);
+  } catch (error) {
+    res.status(500).json({ error: "Error on ranking" });
   }
 };
