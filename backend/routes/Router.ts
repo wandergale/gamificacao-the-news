304d0ef402da85dc32b70a2a09802c2284b9307c
Wanderson notebook <wandersonlife7@gmail.com>
2025-02-20 19:50:21 -0300

    new router metrics

--- backend/routes/Router.ts
+++ backend/routes/Router.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { subscribe } from "../controllers/userController";
 import { processWebhook } from "../controllers/webhookController";
+import { metrics } from "../controllers/dashboardController";
 
 const router = Router();
 
 router.get("/create", subscribe);
 router.get("/webhook", processWebhook);
+router.get("/metrics", metrics);
 
 export default router;
