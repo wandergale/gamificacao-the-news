d016e1e868f3f408743a46bbbd633eb43a9174c6
Wanderson notebook <wandersonlife7@gmail.com>
2025-02-19 22:12:01 -0300

    adding routes

--- backend/routes/Router.ts
+++ backend/routes/Router.ts
@@ -1,15 +1,10 @@
 import { Router } from "express";
-
-import { handleWebhook } from "../controllers/webhookController";
-import { login } from "../controllers/authController";
-import { getDashboard } from "../controllers/dashboardController";
-import { updateStreak } from "../controllers/streakController";
+import { getUsers } from "../controllers/userController";
+import { processWebhook } from "../controllers/webhookController";
 
 const router = Router();
 
-router.post("/webhook", handleWebhook);
-router.get("/login", login);
-router.get("/dashboard", getDashboard);
-router.put("/streak", updateStreak);
+router.get("/users", getUsers);
+router.post("/webhook", processWebhook);
 
 export default router;
