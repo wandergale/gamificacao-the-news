74966303875e736356d01440c1065586301b1fdf
Wanderson notebook <wandersonlife7@gmail.com>
2025-02-20 20:40:30 -0300

    fixing syntax

--- backend/controllers/dashboardController.ts
+++ backend/controllers/dashboardController.ts
@@ -14,9 +14,10 @@ export const topReaders = async (req: Request, res: Response) => {
   try {
     const result = await pool.query(`
             SELECT u.email, s.current_streak, s.longest_streak, s.last_read
-            FROM streaks s 
+                FROM streaks s
                 JOIN users u ON u.id = s.user_id
-                ORDER BY s.current_streak DESC LIMIT 10`);
+                ORDER BY s.current_streak DESC
+                LIMIT 10;`);
 
     res.json(result.rows);
   } catch (error) {
