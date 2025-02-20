2360578f45ee098baddadb7a4bbbe6921687d068
Wanderson notebook <wandersonlife7@gmail.com>
2025-02-20 00:35:54 -0300

    adding router get to webhookHandle

--- backend/routes/Router.ts
+++ backend/routes/Router.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { getUsers } from "../controllers/userController";
-import { processWebhook } from "../controllers/webhookController";
+import {
+  processWebhook,
+  webhookHandler,
+} from "../controllers/webhookController";
 
 const router = Router();
 
 router.get("/users", getUsers);
 router.post("/webhook", processWebhook);
+router.get("/webhook", webhookHandler);
 
 export default router;
