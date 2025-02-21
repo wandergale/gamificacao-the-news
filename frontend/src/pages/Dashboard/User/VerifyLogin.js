4255f78edd3e8624bd4ee4981d3c38726e1a2802
Wanderson notebook <wandersonlife7@gmail.com>
2025-02-21 15:27:30 -0300

    verify login and protected route

---
+++ frontend/src/pages/Dashboard/User/VerifyLogin.js
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+
+export const VerifyLogin = () => {
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const token = searchParams.get("token");
+
+  useEffect(() => {
+    if (token) {
+      localStorage.setItem("token", token);
+      navigate("/dashboard");
+    } else {
+      alert("Invalid token or expired");
+      navigate("/login");
+    }
+  }, [token, navigate]);
+
+  return <p>Verificando login...</p>;
+};
+
+export const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem("token");
+  return token ? children : <Navigate to="/login" />;
+};
