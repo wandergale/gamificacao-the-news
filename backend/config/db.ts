6d9d2dcd1cb0eb1459145f1b0e0f32aefde545e3
Wanderson notebook <wandersonlife7@gmail.com>
2025-02-21 12:40:25 -0300

    fixing some query

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -46,9 +46,7 @@ const createTables = async () => {
         utm_campaign TEXT,
         utm_channel TEXT
       );
-    `);
-
-    await client.query(`
+    
       DROP VIEW IF EXISTS engagement_metrics;
       CREATE VIEW engagement_metrics AS
       SELECT 
