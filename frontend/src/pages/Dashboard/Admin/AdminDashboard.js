466dc432c5194943e80e32a7933df1a212fd6e54
Wanderson notebook <wandersonlife7@gmail.com>
2025-02-23 10:39:56 -0300

    user-dashboard styles

--- frontend/src/pages/Dashboard/Admin/AdminDashboard.js
+++ frontend/src/pages/Dashboard/Admin/AdminDashboard.js
@@ -40,7 +40,7 @@ const AdminDashboard = () => {
 
   return (
     <div className={style.container}>
-      <h2>Painel Administrativo</h2>
+      <h2 id={style.painel}>Painel Administrativo</h2>
 
       <div className={style.general_metrics}>
         <h3>Métricas Gerais</h3>
@@ -69,9 +69,9 @@ const AdminDashboard = () => {
 
       <div>
         <h3>Top 10 Leitores Mais Engajados</h3>
-        <ul>
+        <ul className={style.ul_top}>
           {ranking.map((user, index) => (
-            <li key={index}>
+            <li className={style.li_top} key={index}>
               {user.email} - Streak Atual: {user.current_streak} | Máx:{" "}
               {user.longest_streak}
             </li>
@@ -82,16 +82,26 @@ const AdminDashboard = () => {
       <div>
         <h3>Filtrar Estatísticas</h3>
         <label>Data Início:</label>
-        <input type="date" onChange={(e) => setStartDate(e.target.value)} />
+        <input
+          className={style.input_filter}
+          type="date"
+          onChange={(e) => setStartDate(e.target.value)}
+        />
         <label>Data Fim:</label>
-        <input type="date" onChange={(e) => setEndDate(e.target.value)} />
+        <input
+          className={style.input_filter}
+          type="date"
+          onChange={(e) => setEndDate(e.target.value)}
+        />
         <label>Status:</label>
         <select onChange={(e) => setStreakStatus(e.target.value)}>
           <option value="">Todos</option>
           <option value="active">Ativos</option>
           <option value="inactive">Inativos</option>
         </select>
-        <button onClick={handleFilter}>Filtrar</button>
+        <button className={style.btn_filter} onClick={handleFilter}>
+          Filtrar
+        </button>
       </div>
     </div>
   );
