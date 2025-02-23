5c877dcabe156b6a15684148f3a1dfb149c5e528
Wanderson notebook <wandersonlife7@gmail.com>
2025-02-23 14:48:03 -0300

    fixing dotenv error

--- backend/server.ts
+++ backend/server.ts
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "https://the-news-bice.vercel.app",
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type,Authorization",
   })
